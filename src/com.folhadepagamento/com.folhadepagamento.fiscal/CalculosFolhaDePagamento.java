459aa6bc10d7f3cb7c04a6d09dcf6d62f2047e21
Lucas Jansen <jansenlucas420@gmail.com>
2024-09-12 22:57:04 -0300

    Alteracao na interface calculos folha de pagamento

--- src/com.folhadepagamento/com.folhadepagamento.fiscal/CalculosFolhaDePagamento.java
+++ src/com.folhadepagamento/com.folhadepagamento.fiscal/CalculosFolhaDePagamento.java
@@ -1,4 +1,12 @@
 package com.folhadepagamento.com.folhadepagamento.fiscal;
 
 public interface CalculosFolhaDePagamento {
+	
+	public void calcularSalarioLiquido(Double salarioBruto );
+	
+	public void calcularInss(Double salarioBruto );
+	
+	public void calcularValorPorDependente(Double salarioBruto);
+	
+	public void calcularIR();
 }
