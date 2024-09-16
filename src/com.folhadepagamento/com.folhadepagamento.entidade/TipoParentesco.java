fe942bca5c6841c46154bf484ccdcc80c94e0981
MtAguiar <matheus.saguiar@hotmail.com>
2024-09-16 16:56:04 -0300

    Adicionando alterações na classe LeituradArquivo com os códigos testados e em plena execução.Além disso, adicionei um construtor vazio na classe Funcionário. Criei também um método para buscar os valores do enum e comparar se um deles existe na linha

--- src/com.folhadepagamento/com.folhadepagamento.entidade/TipoParentesco.java
+++ src/com.folhadepagamento/com.folhadepagamento.entidade/TipoParentesco.java
@@ -2,6 +2,29 @@ package com.folhadepagamento.com.folhadepagamento.entidade;
 
 public enum TipoParentesco {
 
-	FILHO, SOBRINHO, OUTROS
+	FILHO("FILHO"),
+	SOBRINHO("SOBRINHO"),
+	OUTROS("OUTROS");
+
+	private String descricao;
+
+	TipoParentesco(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return this.descricao = descricao;
+    }
+
+	public static TipoParentesco obterParentesco(String descricao) {
+		//Criei um método assim para que eu possa chamar na main
+		//e buscar se tem ou não parentesco na linha para poder definir  se é funcionario ou dependente
+		for (TipoParentesco tipoParentesco : TipoParentesco.values()) {
+			if (tipoParentesco.toString().equals(descricao)) {
+				return tipoParentesco;
+			}
+		}
+		return null;
+	}
 	
 }
--- src/com.folhadepagamento/com.folhadepagamento.servico/LeituraArquivo.java
+++ src/com.folhadepagamento/com.folhadepagamento.servico/LeituraArquivo.java
@@ -1,4 +1,90 @@
 package com.folhadepagamento.com.folhadepagamento.servico;
 
+import com.folhadepagamento.com.folhadepagamento.entidade.Dependente;
+import com.folhadepagamento.com.folhadepagamento.entidade.Funcionario;
+import com.folhadepagamento.com.folhadepagamento.entidade.TipoParentesco;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
 public class LeituraArquivo {
+    public static void main(String[] args) throws IOException {
+
+        Scanner lerArquivo = new Scanner((new File("C:\\Users\\Matheus\\Desktop\\Serratec\\Programação Orientada a Objetos\\Códigos\\ProjetoFinalPOO\\serratec-poo-final-project-G1\\src\\com.folhadepagamento\\com.folhadepagamento.resources\\tabela_funcionario.csv")));
+        System.out.println("Digite o nome do arquivo:");
+        //AQUI EU PEÇO NOME DO ARQUIVO PARA O USUÁRIO
+
+        List<String> linhas = new ArrayList<>(); //Criação do Array para armazenar as linhas
+        while (lerArquivo.hasNextLine()) { //Enquanto tiverem linhas no arquivo, cada uma será adicionada na variável
+            linhas.add(lerArquivo.nextLine());
+        }
+        List<Funcionario> funcionarios = new ArrayList<>(); //Criando Array de Funcionarios
+        Funcionario funcionario = new Funcionario(); //Instanciando um funcionário vazio fora do FOR para quando ele rodar começar vazio. Para isso criei o construtor vazio.
+        for (String linha : linhas) {
+            if (!linha.isEmpty()) {
+                String[] dadosDaLinha = linha.split(";"); //Criando array de String para separar as informações de cada linha.
+                //  if (dadosDaLinha.length >= 4) { //
+                int indexFinal = dadosDaLinha.length - 1; //Sem -1 estava pegando a posição errada. Essa foi a única maneira que encontrei para a finalidade da variável indexFinal ser atendida.
+                if (TipoParentesco.obterParentesco(dadosDaLinha[indexFinal]) == null) { //Aqui eu pego...
+                    funcionario.setNome(dadosDaLinha[0]);
+                    funcionario.setCpf(dadosDaLinha[1]);
+
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd"); //Tive que formatar para receber uma string e transformar num LocalDate
+                    LocalDate dataNascimento = LocalDate.parse(dadosDaLinha[2], formatter);
+                    funcionario.setDataNascimento(dataNascimento);
+                    dadosDaLinha[2] = dataNascimento.format(formatter);
+                    //System.out.println(dadosDaLinha[2]);
+                    funcionario.setSalarioBruto(Double.valueOf(dadosDaLinha[3]));
+                    //System.out.println(dadosDaLinha[3]);
+
+                } else {
+
+                    // Trata como dependente
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd"); // Formatação da data
+                    LocalDate dataNascimento = LocalDate.parse(dadosDaLinha[2], formatter);
+
+                    // Obtém o tipo de parentesco da string
+                    TipoParentesco parentesco = TipoParentesco.valueOf(dadosDaLinha[3]);
+
+                    // Instancia o dependente
+                    Dependente dependente = new Dependente(dadosDaLinha[0], dadosDaLinha[1], dataNascimento, parentesco);
+
+                    // Adiciona o dependente ao funcionário
+                    funcionario.adicionarDependente(dependente);
+                    //System.out.println(dependente);
+                }
+            } else {
+                funcionarios.add(funcionario);
+                funcionario = new Funcionario();
+            }
+
+        }
+
+        for (Funcionario f : funcionarios) {
+            System.out.println("Funcionário: " + f.getNome() + " - CPF: " + f.getCpf() + "Data de Nascimento: " +
+                    f.getDataNascimento() + "Salario Bruto: R$" + f.getSalarioBruto());
+
+            List<Dependente> dependentes = f.getDependentes();
+            if (dependentes.isEmpty()) {
+                System.out.println("Esse funcionário não tem dependentes.");
+            } else {
+                System.out.println("Dependentes:");
+                for (Dependente dependente : dependentes) {
+                    System.out.println("  - Nome: " + dependente.getNome() + ", CPF: " + dependente.getCpf() +
+                            ", Data de Nascimento: " + dependente.getDataNascimento() +
+                            ", Parentesco: " + dependente.getTipoParentesco());
+                }
+            }
+
+            lerArquivo.close();
+      }
+
+    }
 }
+
